1198824bda8bd9494b5f2bf36676a2025d58e057
zahra <zahra0a1418@gmail.com>
2023-10-11 09:43:28 +0330

    separation of files

---
+++ functions/backpack.js
@@ -0,0 +1,73 @@
+const pwSort = (a, b) => {
+    const x1 = a.value / a.weight;
+    const x2 = b.value / b.weight;
+    return x2 - x1;
+}
+
+const gcd = (a, b) => {
+    if (b === 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+const divideByFraction = (a, b) => {
+    if (!a || !b) throw new Error('Parameter is not a number!');
+    const clacGcd = gcd(a, b);
+    const numerator = a / clacGcd;
+    const denominator = b / clacGcd;
+    return numerator + (denominator !== 1 ? "/" + denominator : "")
+}
+
+const convertNumberToFraction = (number) => {
+    const decimalIndex = number.toString().indexOf(".");
+    const decimalValue = number.toString().substring(decimalIndex + 1);
+    let denominator = Math.pow(10, decimalValue.length);
+    let numerator = number * denominator;
+    const clacGcd = gcd(numerator, denominator);
+    denominator /= clacGcd;
+    numerator /= clacGcd;
+    return numerator + (denominator !== 1 ? "/" + denominator : "");
+}
+
+const backpack = (totalMass, things) => {
+    things.sort(pwSort);
+    let rc = totalMass;
+    const result = new Map();
+    for (const thing of things) {
+        if (rc === 0) {
+            break;
+        }
+        else if (thing.weight > rc) {
+            const percent = rc / thing.weight;
+            const weightPercent = percent * thing.weight;
+            result.set(thing.value, convertNumberToFraction(percent));
+            rc -= weightPercent;
+        } else {
+            result.set(thing.value, 1);
+            rc -= thing.weight;
+        }
+    }
+
+    return result;
+}
+
+const backpackWithMiddle = (totalMass, things) => {
+    const middle = things[Math.floor(things.length / 2)];
+    let arrayG, arrayE, arrayL = [];
+    let wG, wE, wL = 0;
+    const pwMiddle = middle.value / middle.weight;
+    for (const thing of things) {
+        const pwThing = thing.value / thing.weight;
+        if (pwThing > pwMiddle) {
+            arrayG.push(pwThing);
+            wG += thing.value;
+        } else if (pwThing < pwMiddle) {
+            arrayL.push(pwThing);
+            wL += thing.value;
+        } else {
+            arrayE.push(pwThing);
+            wE += thing.value;
+        }
+    }
+}
